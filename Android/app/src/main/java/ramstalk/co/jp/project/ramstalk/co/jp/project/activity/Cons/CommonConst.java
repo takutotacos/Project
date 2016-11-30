a610535723fc6416ee287efee320ccc9e9599d9d
Takuto <s.takuto0214@gmail.com>
2016-12-01 05:46:39 +0900

    Add Cons and test sharedPreference function

---
+++ Android/app/src/main/java/ramstalk/co/jp/project/ramstalk/co/jp/project/activity/Cons/CommonConst.java
@@ -0,0 +1,13 @@
+package ramstalk.co.jp.project.ramstalk.co.jp.project.activity.Cons;
+
+/**
+ * Created by sugitatakuto on 2016/12/01.
+ */
+public class CommonConst {
+
+    public class FileName {
+
+        public static final String SHARED_PREFERENCES = "Project";
+
+    }
+}

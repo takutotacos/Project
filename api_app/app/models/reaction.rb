afbe63479c6a3f2bff369882a605b7288c569446
Takuto <s.takuto0214@gmail.com>
2017-02-23 17:20:10 +0900

    タイムラインで画像を表示する時にGlideを使用。 Railsで不必要なモデルとそれに関係するファイルを削除。

--- api_app/app/models/reaction.rb
+++
@@ -1,4 +0,0 @@
-class Reaction < ApplicationRecord
-  belongs_to :posting, counter_cache: :likes_count
-  belongs_to :user
-end
--- api_app/app/serializers/api/posting_serializer.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  class PostingSerializer < ActiveModel::Serializer
-    attributes :id, :comment, :image, :latitude, :longitude, :location1, :location2,
-     :created_at, :updated_at, :category, :user
-
-    has_one :user, serializer: UserSerializer
-    has_one :category
-  end
-end
